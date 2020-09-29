b1ccee2351c3ce9ff3fad38aaeeecdb450fc853e
kartiknawal <kartiknawal.7@gmail.com>
2020-09-29 14:32:30 +0530

    [kartiknawal] add calculating employee wage using switch

--- Program.cs
+++ Program.cs
@@ -4,29 +4,27 @@ namespace Employee_Wages
 {
     class Program
     {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args)
-        {
-            //Constants
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
-
+        {       
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             //Computation
-            int empcheck = random.Next(0, 3);
-            if (empcheck == IS_PART_TIME)
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
             {
-                empHrs = 4;
-            }
-            else if (empcheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-            }
-            else
-            {
-                empHrs = 0;
+                case 0:
+                    empHrs = 0;
+                    break;
+                case 1:
+                    empHrs = 4;
+                    break;
+                case 2:
+                    empHrs = 8;
+                    break;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage: " + empWage);
