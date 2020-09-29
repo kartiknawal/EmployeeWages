8d837389114599a427422efeab761f1b8149f79e
kartiknawal <kartiknawal.7@gmail.com>
2020-09-29 14:16:02 +0530

    [kartiknawal] add part time employee

--- Program.cs
+++ Program.cs
@@ -7,15 +7,20 @@ namespace Employee_Wages
         static void Main(string[] args)
         {
             //Constants
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
 
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             //Computation
-            int empcheck = random.Next(0, 2);
-            if (empcheck == IS_FULL_TIME)
+            int empcheck = random.Next(0, 3);
+            if (empcheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empcheck == IS_FULL_TIME)
             {
                 empHrs = 8;
             }
