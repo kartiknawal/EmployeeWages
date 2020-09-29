2b7214866a080f87328f586e8b0dfd1ad7ec37fa
kartiknawal <kartiknawal.7@gmail.com>
2020-09-29 14:08:40 +0530

    [kartiknawal] add calculating employee wage

--- Program.cs
+++ Program.cs
@@ -8,17 +8,23 @@ namespace Employee_Wages
         {
             //Constants
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             //Computation
             int empcheck = random.Next(0, 2);
             if (empcheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is Present");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage: " + empWage);
         }
     }
 }
