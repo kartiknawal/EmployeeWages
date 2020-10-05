51ebefcd768fc0170c30457ffee9d24d126109e3
kartiknawal <kartiknawal.7@gmail.com>
2020-10-05 08:29:48 +0530

    [kartiknawal] refractor.manage employee wage of multiple companies

--- Program.cs
+++ Program.cs
@@ -6,8 +6,11 @@ namespace EmployeeWages
     {
         static void Main(String[] args)
         {
-            ForMultipleCompanies.computeEmpWage("Google", 25, 3, 10);
-            ForMultipleCompanies.computeEmpWage("Facebook", 11, 4, 20);
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("Google", 25, 3, 10);
+            empWageBuilder.addCompanyEmpWage("Facebook", 11, 4, 20);
+            empWageBuilder.computeEmpWage();
         }
     }
 }
+
